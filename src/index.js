b742fada3720f12ffdbeb57f7d20a2919a1af493
aram <v.shadunts@gmail.com>
2020-04-27 15:57:06 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Arrr, {Appa} from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Appa></Appa>
+    <Arrr />
   </React.StrictMode>,
   document.getElementById('root')
 );
